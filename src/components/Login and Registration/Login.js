0dd5bc62e0cbf58b5e16d1f485d3d91a053d52d9
FishySenpai <akanomanbasit@gmail.com>
2022-12-02 09:11:56 +0500

    finished login/registration

--- src/components/Login and Registration/Login.js
+++ src/components/Login and Registration/Login.js
@@ -1,35 +1,62 @@
 import React from 'react'
+import { useState } from 'react';
+import Axios from 'axios';
 
 const Login = () => {
+  const [email, setEmail] = useState();
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
+  const login = () => {
+    Axios.post("http://localhost:3001/login", {
+      email: email,
+      username: username,
+      password: password,
+    }).then((response) => {
+      console.log(response);
+    });
+  };
   return (
     <div className="relative flex flex-col justify-center min-h-screen ">
       <div className="w-full p-6 m-auto mt-36 bg-gray-500 rounded-md shadow-md lg:max-w-xl">
         <h1 className="text-3xl font-mono cursor-pointer text-white underline">
           Login
         </h1>
-        <form className="mt-6">
+       
           <div className="mb-2">
-            <label
-              for="email"
-              className="font-mono text-[16px] text-white"
-            >
+            <label for="email" className="font-mono text-[16px] text-white">
               Email
             </label>
             <input
               type="email"
               className="block w-full px-4 py-2 mt-2 font-mono text-gray-900 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
             />
           </div>
           <div className="mb-2">
-            <label
-              for="password"
-              className="font-mono text-[16px] text-white"
-            >
+            <label for="username" className="font-mono text-[16px] text-white">
+              Username
+            </label>
+            <input
+              type="text"
+              className="block w-full px-4 py-2 mt-2 font-mono text-gray-900 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+            />
+          </div>
+          <div className="mb-2">
+            <label for="password" className="font-mono text-[16px] text-white">
               Password
             </label>
             <input
               type="password"
               className="block w-full px-4 py-2 mt-2 text-gray-800 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
             />
           </div>
           <a
@@ -39,11 +66,13 @@ const Login = () => {
             Forgot Password?
           </a>
           <div className="mt-6">
-            <button className="w-full px-4 py-2 tracking-wide font-mono cursor-pointer text-[16px] text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-800 focus:outline-none focus:bg-gray-900">
+            <button
+              className="w-full px-4 py-2 tracking-wide font-mono cursor-pointer text-[16px] text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-800 focus:outline-none focus:bg-gray-900"
+              onClick={login}
+            >
               Login
             </button>
           </div>
-        </form>
 
         <p className="mt-8 font-mono cursor-pointer text-[12px] text-white">
           {" "}
