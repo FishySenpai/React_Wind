188cf9de070f32082818e2efdb3d08b1e62e2964
FishySenpai <akanomanbasit@gmail.com>
2023-02-03 19:26:46 +0500

    carousel

--- src/components/Seasons/Upcoming.js
+++ src/components/Seasons/Upcoming.js
@@ -3,7 +3,7 @@ import { useFetch } from "../Getdata";
 import { Link } from "react-router-dom";
 
 const Upcoming = () => {
-  const [viewAll, setViewAll] = useState(6);
+  const [viewAll, setViewAll] = useState();
   const url = `https://api.jikan.moe/v4/seasons/upcoming`;
   const { topAnime, loading } = useFetch(url);
   console.log(topAnime);
@@ -22,22 +22,25 @@ const Upcoming = () => {
               View All
             </button>
           </div>
+
           <div className="md:px-6 items-center container justify-between">
             <ul className="flex flex-wrap">
-              {topAnime.slice(0, viewAll).map((top, index) => (
-                <li className="mr-4 md:mr-8 pb-6" key={top.mal_id}>
-                  <a href={`/topanime/${top.mal_id}`}>
-                    <img
-                      className="w-[140px] h-[220px] md:w-[188px] md:h-[264px] rounded hover:shadow-lg cursor-pointer hover:scale-105"
-                      src={top.images.jpg.large_image_url}
-                      alt="img"
-                    />
-                  </a>
-                  <div className="w-36 md:w-48 text-gray-500 text-lg text hover:text-red-500 cursor-pointer">
-                    <Link to={`/topanime/${top.mal_id}`}>{top.title}</Link>
-                  </div>
-                </li>
-              ))}
+              <div className="flex flex-row my-14 overflow-auto animescrollbar">
+                {topAnime.slice(0, viewAll).map((top, index) => (
+                  <li className="mr-4 md:mr-8 pb-6" key={top.mal_id}>
+                    <a href={`/topanime/${top.mal_id}`}>
+                      <img
+                        className="w-[140px] h-[220px] md:w-[188px] md:h-[264px] rounded hover:shadow-lg cursor-pointer hover:scale-105"
+                        src={top.images.jpg.large_image_url}
+                        alt="img"
+                      />
+                    </a>
+                    <div className="w-36 md:w-48 text-gray-500 text-lg text hover:text-red-500 cursor-pointer">
+                      <Link to={`/topanime/${top.mal_id}`}>{top.title}</Link>
+                    </div>
+                  </li>
+                ))}
+              </div>
             </ul>
           </div>
         </div>
