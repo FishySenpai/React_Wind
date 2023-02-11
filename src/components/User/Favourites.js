495fcaf4d507e8e0768903029bea4fe1af37e465
FishySenpai <akanomanbasit@gmail.com>
2023-02-11 22:58:26 +0500

    dafa

--- src/components/User/Favourites.js
+++ src/components/User/Favourites.js
@@ -1,32 +1,34 @@
 import React, { useState, useContext, useEffect } from "react";
 import { UserProfile } from "../Contexts/UserProfile";
 import { db } from "../../firebaseConfig";
-
+import { useFetch } from "../Getdata";
 import { doc, getDocs, collection } from "firebase/firestore";
 import { async } from "@firebase/util";
 
 
 const Favourites = () => {
   const docRef = collection(db, "fav");
-
+  const url = `https://api.jikan.moe/v4/anime/${currentMal}`;
+  const { topAnime, loading } = useFetch(url);
   const [data, setData] = useState([]);
-  
+  const [currentMal, setCurrentMal] = useState();
 useEffect(()=>{
 const docSnap = async () => {
   const favs = await getDocs(docRef);
   setData(favs.docs.map((doc) => ({ ...doc.data()})));
+  {
+    data.map((fa) => {
+      return <div>{setCurrentMal(fa.mal_id)}</div>;
+    });
+  }
 };
 docSnap();
+
 }, [])
+useFetch();
   return <div className="text-black">
     
-    {data.map((fa)=>{
-      return (
-        <div>
-          {fa.mal_id}
-          </div>
-      )
-    })}
+    
     </div>;
 };
 
