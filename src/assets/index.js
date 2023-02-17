4cb37440f4130f0994ce4efa568a9188f4125fd2
FishySenpai <akanomanbasit@gmail.com>
2023-02-17 18:38:33 +0500

    added google icon

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,7 +2,8 @@ import img1 from './img1.png'
 import menu from './menu.svg'
 import close from "./close.svg";
 import instagram from "./instagram.svg"
-import google from "./google.svg"
+import google from "./google.png"
+
 export {
     img1,
     menu,
