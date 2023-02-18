5a20a1ae8c44975fd9c676f57464ab1e1b489d07
FishySenpai <akanomanbasit@gmail.com>
2023-02-18 11:28:41 +0500

    added fixes

--- src/components/User/Favourites.js
+++ src/components/User/Favourites.js
@@ -5,11 +5,10 @@ import { auth } from "../../firebaseConfig";
 import { doc, getDocs, collection, where } from "firebase/firestore";
 import { Link, useNavigate } from "react-router-dom";
 const Favourites = () => {
-  const [currentMal, setCurrentMal] = useState([]);
-  const [data, setData] = useState({});
+  const [data, setData] = useState([]); //data is returned back in [] 
   const [user, setUser] = useState({});
   const navigate = useNavigate();
-  
+
   useEffect(() => {
     onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
@@ -17,20 +16,25 @@ const Favourites = () => {
     });
   }, [user]);
   useEffect(() => {
-    if(user.uid){
-    const docRef = collection(db, "users", user.uid, "favs");
-    
-    const docSnap = async () => {
-      const fav = await getDocs(docRef);
-      setData(fav.docs.map((doc)=>({...doc.data(), id:doc.id})));
-      console.log(data);
-    };
-    docSnap();}
+    if (user.uid) {
+      try {
+        const docRef = collection(db, "users", user.uid, "favs");
+
+        const docSnap = async () => {
+          const fav = await getDocs(docRef);
+          setData(fav.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+          console.log(data);
+        };
+        docSnap();
+      } catch (err) {
+        console.log(err);
+      }
+    }
   }, [user.uid]); //use dependency list so useEffect only runs when there is change to useState
   return (
     <div className="px-6 items-center mx-auto container justify-between">
       <div className="sm:p-6 pt-12 items-center container justify-between">
-        {data.map((top)=>(top.topAnime.title))}
+        {data.map((top) => top.topAnime?.title)}
       </div>
     </div>
   );
