0218ee794198ad84f03622bcceda8bd5a0251ee5
FishySenpai <akanomanbasit@gmail.com>
2023-02-18 11:33:02 +0500

    finalized styling of fav

--- src/components/User/Favourites.js
+++ src/components/User/Favourites.js
@@ -34,7 +34,29 @@ const Favourites = () => {
   return (
     <div className="px-6 items-center mx-auto container justify-between">
       <div className="sm:p-6 pt-12 items-center container justify-between">
-        {data.map((top) => top.topAnime?.title)}
+        <ul className="flex flex-wrap">
+          {data.map((top) => (
+            <li className="mr-4 md:mr-8 pb-6" key={top.topAnime.mal_id}>
+              <a href={`/topanime/${top.topAnime.mal_id}`}>
+                <img
+                  className="w-[140px] h-[220px] md:w-[188px] md:h-[264px] rounded hover:shadow-lg cursor-pointer hover:scale-105"
+                  src={top.topAnime.images?.jpg.large_image_url}
+                  alt="img"
+                />
+              </a>
+              <div className="w-36 md:w-48 text-gray-500 text-lg hover:text-red-500 cursor-pointer">
+                <button>
+                  <Link
+                    className="text"
+                    to={`/topanime/${top.topAnime.mal_id}`}
+                  >
+                    {top.topAnime.title}
+                  </Link>
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
