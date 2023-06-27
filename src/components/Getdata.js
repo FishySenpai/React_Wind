870c5e2359b94995ec975c85afd8392b7f734290
FishySenpai <akanomanbasit@gmail.com>
2023-06-27 21:16:42 +0500

    error handling

--- src/components/Getdata.js
+++ src/components/Getdata.js
@@ -1,22 +1,40 @@
-import React, { useState, useEffect } from "react";
-export  const useFetch= (...url)=>{
-  const [loading, setLoading] = useState(true)
+import React, {useState, useEffect} from "react";
+export const useFetch = (...url) => {
+  const [loading, setLoading] = useState(true);
   const [topAnime, setTopAnime] = useState([]);
+  const [retry, setRetry] = useState(0);
+  const maxRetries = 3; // Maximum number of retries
 
   const getTopAnime = async () => {
     const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
+    }
     const topAnime = await response.json();
     setTopAnime(topAnime.data);
-    setLoading(false)
+    setLoading(false);
   };
-  useEffect(() => {
-    try{
-      getTopAnime();
-    } catch(err){
-      console.log(err)
-    }
-    
-  }, []);
-  return {loading, topAnime}
-}
 
+  const fetchData = async () => {
+    try {
+      await getTopAnime();
+    } catch (err) {
+      if (retry < maxRetries) {
+        setRetry(retry + 1);
+        console.log(err);
+        setTimeout(() => {
+          fetchData();
+        }, 2000); // Retry after 2 seconds
+      } else {
+        console.log('Maximum retry count reached');
+        setLoading(false);
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [retry]);
+
+  return { loading, topAnime };
+};
--- src/components/Seasons/ShowSeasons.js
+++ src/components/Seasons/ShowSeasons.js
@@ -5,44 +5,45 @@ import { Link } from "react-router-dom";
 const ShowSeasons = ({loading, topAnime, viewAll}) => { 
   console.log(topAnime);
 
-  if (topAnime) {
-     return (
-       <div className="px-6 items-center mx-auto container justify-between">
-         <div className="md:px-10 items-center mx-auto container justify-between">
-           <div className="md:px-6 items-center container justify-between">
-             <ul className="flex flex-wrap">
-               {topAnime?.slice(0, viewAll).map((top, index) => (
-                 <li className="mr-4 md:mr-8 pb-6" key={top.mal_id}>
-                   <a href={`/topanime/${top.mal_id}`}>
-                     <img
-                       className="w-[140px] h-[220px] md:w-[188px] md:h-[264px] rounded hover:shadow-lg cursor-pointer hover:scale-105"
-                       src={top.images.jpg.large_image_url}
-                       alt="img"
-                     />
-                   </a>
-                   <div className="w-36 md:w-48 text-gray-500 text-lg text hover:text-red-500 cursor-pointer">
-                     <Link to={`/topanime/${top.mal_id}`}>{top.title}</Link>
-                   </div>
-                 </li>
-               ))}
-             </ul>
-           </div>
-         </div>
-       </div>
-     );
-  } else if(loading) {
+  if (loading) {
     return (
       <div className="px-6 items-center mx-auto container justify-between">
         <div className="md:px-16 items-center mx-auto container justify-between">
           <div className="flex flex-wrap">
             {[...Array(viewAll)].map((_, index) => (
-              <div className="  rounded-lg p-1 animate-pulse mr-5" key={index}>
+              <div className="  rounded-lg p-1 animate-pulse mr-7" key={index}>
                 <div className="w-[140px] h-[220px] md:w-[188px] md:h-[264px] bg-gray-300 mb-2"></div>
                 <div className="mt-2">
                   <div className="w-12 md:w-28  bg-gray-300 h-4 shadow"></div>
                 </div>
               </div>
             ))}
+          </div>
+        </div>
+      </div>
+    );
+     
+  } else {
+    return (
+      <div className="px-6 items-center mx-auto container justify-between">
+        <div className="md:px-10 items-center mx-auto container justify-between">
+          <div className="md:px-6 items-center container justify-between">
+            <ul className="flex flex-wrap">
+              {topAnime?.slice(0, viewAll).map((top, index) => (
+                <li className="mr-4 md:mr-8 pb-6" key={top.mal_id}>
+                  <a href={`/topanime/${top.mal_id}`}>
+                    <img
+                      className="w-[140px] h-[220px] md:w-[188px] md:h-[264px] rounded hover:shadow-lg cursor-pointer hover:scale-105"
+                      src={top.images.jpg.large_image_url}
+                      alt="img"
+                    />
+                  </a>
+                  <div className="w-36 md:w-48 text-gray-500 text-lg text hover:text-red-500 cursor-pointer">
+                    <Link to={`/topanime/${top.mal_id}`}>{top.title}</Link>
+                  </div>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
